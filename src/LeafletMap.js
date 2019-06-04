e3ff9e27e3883acd6591d96391870bb4e54ddf63
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-06-03 20:26:42 -0400

    Update centering of map viewport. Add arbitrary markers

--- src/LeafletMap.js
+++ src/LeafletMap.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, type Viewport } from 'react-leaflet'
+import { Map, TileLayer, Marker, type Viewport } from 'react-leaflet'
 import './map.css';
 
 const DEFAULT_VIEWPORT = {
-  center: [11.933773, 7.898987],
-  zoom: 2.7,
+  center: [6.662966, 7.690188],
+  zoom: 2.7
 }
 
 export default class LeafletMap extends Component<{}, { viewport: Viewport }> {
@@ -31,6 +31,9 @@ export default class LeafletMap extends Component<{}, { viewport: Viewport }> {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+          <Marker position={[49.8397, 24.0297]} />
+          <Marker position={[52.2297, 21.0122]} />
+          <Marker position={[51.5074, -0.0901]} />
       </Map>
     )
   }
