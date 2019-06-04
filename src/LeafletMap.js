f1c60d7ae6d122b40b6e20cef44746baf7fe95e1
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-06-03 20:51:43 -0400

    Updated example to use map cluster component

--- src/LeafletMap.js
+++ src/LeafletMap.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, type Viewport } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+
+import 'react-leaflet-markercluster/dist/styles.min.css';
 import './map.css';
 
 const DEFAULT_VIEWPORT = {
@@ -7,9 +10,13 @@ const DEFAULT_VIEWPORT = {
   zoom: 2.7
 }
 
-export default class LeafletMap extends Component<{}, { viewport: Viewport }> {
-  state = {
-    viewport: DEFAULT_VIEWPORT,
+export default class LeafletMap extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      viewport: DEFAULT_VIEWPORT,
+    }
   }
 
   onClickReset = () => {
@@ -26,14 +33,16 @@ export default class LeafletMap extends Component<{}, { viewport: Viewport }> {
         onClick={this.onClickReset}
         onViewportChanged={this.onViewportChanged}
         viewport={this.state.viewport}
-        zoomSnap="0.1">
+        zoomSnap="0.2">
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <MarkerClusterGroup>
           <Marker position={[49.8397, 24.0297]} />
           <Marker position={[52.2297, 21.0122]} />
           <Marker position={[51.5074, -0.0901]} />
+        </MarkerClusterGroup>
       </Map>
     )
   }
