06033b04ee01c9adf7eb9fe3c58e4eca55221089
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-06-04 11:18:53 -0400

    Add location name as Marker/Popup

--- src/LeafletMap.js
+++ src/LeafletMap.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Map, TileLayer, Marker, type Viewport } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup, type Viewport } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 
 import { getLocations } from "./api";
@@ -15,7 +15,7 @@ const DEFAULT_VIEWPORT = {
 export default class LeafletMap extends React.Component {
   state = {
     viewport: DEFAULT_VIEWPORT,
-    locations: []
+    locations: [],
   };
 
   componentDidMount() {
@@ -26,9 +26,7 @@ export default class LeafletMap extends React.Component {
         locationList.push(result[i]);
       }
 
-      this.setState({
-        locations: locationList
-      });
+      this.setState({ locations: locationList });
     });
   }
 
@@ -57,7 +55,9 @@ export default class LeafletMap extends React.Component {
             <Marker
               key={loc["location_id"]}
               position={[parseFloat(loc["lat"]), parseFloat(loc["lng"])]}
-            />
+            >
+              <Popup><span>{loc["name"]}</span></Popup>
+            </Marker>
           ))}
         </MarkerClusterGroup>
       </Map>
