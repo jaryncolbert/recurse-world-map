d7adab1bc1d845cb187610fcf6b6470656032f33
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-07-16 15:31:49 -0400

    Upgrade to Bootstrap4

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./css/index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
