3839b4b7eb451a5f98176b54537b626c4d14e3a3
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-08-09 00:46:18 -0400

    Add navbar using routes for login. Add footer and update styling.

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import GitHubLogo from "../assets/GitHub-Mark-32px.png";
+
+export default function Footer() {
+    return (
+        <footer className="footer mt-auto py-2">
+            <div className="footer-container container-fluid">
+                <a
+                    href="http://www.recurse.com"
+                    title="Made with love at the Recurse Center">
+                    <img
+                        alt="Recurse Center Logo"
+                        src="https://cloud.githubusercontent.com/assets/2883345/11322973/9e557144-910b-11e5-959a-8fdaaa4a88c5.png"
+                        height="14px"
+                    />
+                </a>
+                <span className="divider">|</span>
+                <a href="https://github.com/jaryncolbert/recurse-world-map">
+                    <img src={GitHubLogo} alt="Fork on Github" />
+                    Fork on GitHub
+                </a>
+            </div>
+        </footer>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Link, HashRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import MapIcon from "./assets/map-icon-192x192.png";
+import Footer from "./components/Footer";
 
+// Bootstrap dependencies
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./css/index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const router = (
+    <Router basename="/">
+        <nav className="navbar navbar-light navbar-expand-sm justify-content-sm-between">
+            <Link to="/" className="navbar-brand">
+                <img src={MapIcon} alt="RC Map Icon" />
+                World of Recurse Map
+            </Link>
+
+            <div className="nav-separator">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbar-top"
+                    aria-controls="navbar-top"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbar-top">
+                    <ul className="navbar-nav mt-2 mt-lg-0">
+                        <NavListItem path="/login" text="Login" />
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <Route exact path="/" component={App} />
+
+        <Footer />
+    </Router>
+);
+
+function NavListItem({ path, text }) {
+    return (
+        <li
+            className="nav-item"
+            data-toggle="collapse"
+            data-target=".navbar-collapse.show">
+            <Link to={path} className="nav-link">
+                {text}
+            </Link>
+        </li>
+    );
+}
+
+ReactDOM.render(router, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
