d28a2aaa469ddd308afe6a98551161a3fd1e8134
Jaryn Colbert <jaryn.colbert@gmail.com>
2019-09-01 22:05:41 -0400

    Hide footer on medium and smaller viewports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,24 @@ import React from "react";
 import GitHubLogo from "../assets/GitHub-Mark-32px.png";
 
 export default function Footer() {
-    return (
-        <footer className="footer mt-auto py-2">
-            <div className="footer-container container-fluid">
-                <a
-                    href="http://www.recurse.com"
-                    title="Made with love at the Recurse Center">
-                    <img
-                        alt="Recurse Center Logo"
-                        src="https://cloud.githubusercontent.com/assets/2883345/11322973/9e557144-910b-11e5-959a-8fdaaa4a88c5.png"
-                        height="12px"
-                    />
-                </a>
-                <span className="divider">|</span>
-                <a href="https://github.com/jaryncolbert/recurse-world-map">
-                    <img src={GitHubLogo} alt="Fork on Github" height="16px" />
-                    Fork on GitHub
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer mt-auto py-2 d-none d-lg-block">
+      <div className="footer-container container-fluid">
+        <a
+          href="http://www.recurse.com"
+          title="Made with love at the Recurse Center">
+          <img
+            alt="Recurse Center Logo"
+            src="https://cloud.githubusercontent.com/assets/2883345/11322973/9e557144-910b-11e5-959a-8fdaaa4a88c5.png"
+            height="12px"
+          />
+        </a>
+        <span className="divider">|</span>
+        <a href="https://github.com/jaryncolbert/recurse-world-map">
+          <img src={GitHubLogo} alt="Github logo" height="16px" />
+          Fork on GitHub
+        </a>
+      </div>
+    </footer>
+  );
 }
